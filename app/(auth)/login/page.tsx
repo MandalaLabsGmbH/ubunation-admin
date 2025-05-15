9fa897774d03027ef379e8104c16c5774a49f0e8
Lawson <13117243+sonomar@users.noreply.github.com>
2025-05-15 11:47:58 +0200

    add login and signup page base styles and install nextauth

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import Link from 'next/link'
+
+export default function LoginPage() {
+ return (
+  <Card className='mx-auto max-w-sm border-0 shadow-none mt-4 sm:mt-12 sm:border sm:shadow-sm md:mt-20 lg:mt-24 xl:mt-28'>
+   <CardHeader>
+    <CardTitle className='text-2xl'>Login</CardTitle>
+    <CardDescription>Enter your email below to login to your account</CardDescription>
+   </CardHeader>
+   <CardContent>
+    <div className='grid gap-4'>
+     <div className='grid gap-2'>
+      <Label htmlFor='email'>Email</Label>
+      <Input id='email' name='email' placeholder='m@example.com' />
+     </div>
+     <div className='grid gap-2'>
+      <div className='flex items-center'>
+       <Label htmlFor='password'>Password</Label>
+       <Link href='/forgot' className='ml-auto inline-block text-sm underline'>
+        Forgot your password?
+       </Link>
+      </div>
+      <Input id='password' name='password' type='password' />
+     </div>
+     <Button>Login</Button>
+     <Button variant='outline'>Login with Google</Button>
+    </div>
+    <div className='mt-4 text-center text-sm'>
+     Don&apos;t have an account?{' '}
+     <Link href='/register' className='underline'>
+      Sign up
+     </Link>
+    </div>
+   </CardContent>
+  </Card>
+ )
+}
