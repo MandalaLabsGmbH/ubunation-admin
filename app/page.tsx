080e3055f091d9bebb8b6be4b7efdb774f73390a
Lawson <13117243+sonomar@users.noreply.github.com>
2025-07-16 12:00:54 +0200

    update home page view

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 export default function AdminPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-4">Ubunation Admin Toolset</h1>
-      <p className="text-lg">
-        Use the menu on the left to update and modify content for Ubunation. This page is for administrator users only.
+      <h1 className="text-3xl font-bold mb-4">UBUNΛTION ADMIN TOOLSET</h1>
+      <p className="text-xl pt-10">
+        Use the menu on the left to update and modify content for UBUNΛTION apps.
+      </p>
+       <p className="text-lg pt-10">
+        This page is for administrator use only.
       </p>
     </div>
   );
